b0032c4a6c28658d610156b354727a166dc446e0
Nikites <rockstar.gg@mail.ru>
2023-04-24 23:54:07 +0300

    Add project files.

---
+++ DemoExam/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using DemoExam.ADOApp;
+using System.Windows;
+
+namespace DemoExam
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private static readonly LanguageSchoolEntities _connection = new LanguageSchoolEntities();
+        public static LanguageSchoolEntities Connection => _connection;
+        public static bool IsAdministratorMode { get; set; } = false;
+        public static Visibility AdminVisibility => IsAdministratorMode ? Visibility.Visible : Visibility.Collapsed;
+    }
+}
